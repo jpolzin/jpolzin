602d95fdca0a47430593718845a21c232c60bc68
jpolzin <jpolzin@umich.edu>
2019-10-12 17:20:19 -0400

    homepage refactor in progress

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+
+var data = JSON.parse(fs.readFileSync('./public/data/data.json'));
 
 /* GET about page. */
 function about(req, res, next)
 {
-  res.render('index', { 
-    title: 'joshua polzin | about', 
-    page: 'about',
+  console.log(data.education.coursework)
+  res.render('index', {
+    title: 'joshua polzin | home',
+    page: 'home',
     img_portrait: '/images/josh.jpg',
-    facts: [
-      {
-        name: 'Education',
-        content: "University of Michigan College of Engineering"
-      },
-      {
-        name: 'Graduation',
-        content: 'December 2019'
-      },
-      {
-        name: 'Major',
-        content: 'Computer Science'
-      },
-      {
-        name: 'Minor',
-        content: 'Math'
-      }
-    ],
-    personal_statement: "Hi I'm Josh!"
+    projects: data.projects.slice(0, 4),
+    education_summary: data.filler,
+    coursework: data.education.coursework
   });
 }
 router.get('/', function(req, res, next) {
@@ -89,7 +77,7 @@ router.get('/experience/', function(req, res, next) {
         date: 'May 2018 - August 2018',
         category: 'Protolabs, Inc.',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum luctus iaculis dignissim. ' +
-          'Pellentesque metus erat, cursus a dolor in, lobortis fringilla urna. Suspendisse hendrerit metus congue ex ' + 
+          'Pellentesque metus erat, cursus a dolor in, lobortis fringilla urna. Suspendisse hendrerit metus congue ex ' +
           'accumsan, id tincidunt tortor lacinia. Duis vulputate blandit eros quis lacinia. Aliquam aliquet diam vel iaculis fermentum.'
       }
     ]
